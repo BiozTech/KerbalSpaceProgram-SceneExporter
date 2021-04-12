6d859773a6added3a7083af1e7657236b0da6dbb
BiozTech <61115267+BiozTech@users.noreply.github.com>
2021-04-12 17:53:24 +0300

    Pre-Alpha 0.1

---
+++ GUI.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace SceneExporter
+{
+
+	[KSPAddon(KSPAddon.Startup.Instantly, true)]
+
+	public class SceneExporterGUI : MonoBehaviour
+	{
+
+		static string strKeyRecord, strDirectory;
+		static bool isStrKeyRecordOk, isStrDirectoryOk;
+
+		static bool isGUIVisible = true;
+
+		static Rect windowPosition = new Rect(0.25f * Screen.width, 0.15f * Screen.height, 0, 0);
+
+		void OnGUI()
+		{
+			if (isGUIVisible)
+			{
+				GUIStyle windowStyle = new GUIStyle(GUI.skin.window);
+				windowStyle.padding = new RectOffset(11, 15, 20, 11);
+				windowPosition = GUILayout.Window(("SceneExporterGUI").GetHashCode(), windowPosition, OnWindow, "SceneExporter Settings", windowStyle);
+			}
+		}
+
+		void OnWindow(int windowId)
+		{
+
+			GUILayoutOption[] textFieldLayoutOptions = { GUILayout.Width(200f), GUILayout.Height(20f) };
+			GUIStyle textFieldStyle = new GUIStyle(GUI.skin.textField);
+			//GUIStyle boxStyle = new GUIStyle(GUI.skin.box);
+			//boxStyle.alignment = TextAnchor.MiddleLeft;
+			//boxStyle.padding = new RectOffset(3, 3, 3, 3);
+			GUIStyle toggleStyle = new GUIStyle(GUI.skin.toggle);
+			toggleStyle.margin = new RectOffset(4, 4, 4, 8);
+			GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+			buttonStyle.margin = new RectOffset(4, 4, 13, 4);
+
+			GUILayout.BeginVertical(GUILayout.Width(300f));
+
+			GUILayout.Space(6f);
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Recording");
+			if (Settings.IsEnabled != GUILayout.Toggle(Settings.IsEnabled, Settings.IsEnabled ? " Enabled" : " Disabled", textFieldLayoutOptions))
+			{
+				Settings.IsEnabled = !Settings.IsEnabled;
+			}
+			GUILayout.EndHorizontal();
+			GUILayout.Space(5f);
+
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Exporting textures");
+			if (Settings.IsExportingTextures != GUILayout.Toggle(Settings.IsExportingTextures, Settings.IsExportingTextures ? " Enabled" : " Disabled", textFieldLayoutOptions))
+			{
+				Settings.IsExportingTextures = !Settings.IsExportingTextures;
+			}
+			GUILayout.EndHorizontal();
+
+			GUILayout.BeginHorizontal();
+			GUILayout.Label("Key");
+			ColorizeTextField(textFieldStyle, isStrKeyRecordOk);
+			strKeyRecord = GUILayout.TextField(strKeyRecord, textFieldStyle, textFieldLayoutOptions);
+			GUILayout.EndHorizontal();
+
+			GUILayout.Label("Exported scenes directory");
+			ColorizeTextField(textFieldStyle, isStrDirectoryOk);
+			strDirectory = GUILayout.TextField(strDirectory, textFieldStyle);
+
+			GUILayout.BeginHorizontal();
+			GUILayout.FlexibleSpace();
+			if (GUILayout.Button("Apply", buttonStyle, GUILayout.Width(125f)))
+			{
+
+				bool isOk = true;
+
+				KeyCode key;
+				isStrKeyRecordOk = Enum.TryParse(strKeyRecord, out key);
+				if (isStrKeyRecordOk) Settings.KeyRecord = key;
+				isOk &= isStrKeyRecordOk;
+
+				if (strDirectory != string.Empty)
+				{
+					isStrDirectoryOk = Directory.Exists(strDirectory);
+					if (isStrDirectoryOk) Settings.ScenesDirectory = strDirectory;
+					isOk &= isStrDirectoryOk;
+				} else {
+					isStrDirectoryOk = true;
+					Settings.ScenesDirectory = Settings.ScenesDirectoryDefault;
+				}
+
+				if (isOk) Settings.Save();
+
+			}
+			GUILayout.FlexibleSpace();
+			GUILayout.EndHorizontal();
+
+			GUILayout.EndVertical();
+
+			GUI.DragWindow();
+
+		}
+
+		static void ColorizeTextField(GUIStyle style, bool isStrOk)
+		{
+			style.normal.textColor = style.focused.textColor = style.hover.textColor = (isStrOk) ? Color.white : Color.red;
+		}
+
+		public static void Toggle()
+		{
+			isGUIVisible = !isGUIVisible;
+		}
+
+		public static void Initialize()
+		{
+			strKeyRecord = Settings.KeyRecord.ToString();
+			strDirectory = Settings.ScenesDirectoryToOutput();
+			isStrKeyRecordOk = isStrDirectoryOk = true;
+		}
+
+		void Awake()
+		{
+			DontDestroyOnLoad(gameObject);
+			Initialize();
+		}
+
+	}
+
+}
---
+++ SceneExporter.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+namespace SceneExporter
+{
+
+	[KSPAddon(KSPAddon.Startup.Instantly, true)]
+
+	public class SceneExporter : MonoBehaviour
+	{
+
+		static string activeDirectory = Settings.ScenesDirectory;
+		static string texturesDirectory = Path.Combine(activeDirectory, "textures");
+		static StringBuilder sb = new StringBuilder();
+		static StringBuilder sbMaterials = new StringBuilder();
+		static HashSet<string> materialsCache = new HashSet<string>();
+		static int vertexIndexOffset, gameObjectIndex, textureIndex;
+
+		void Update()
+		{
+			if (Input.GetKeyDown(Settings.KeyRecord))
+			{
+				if (Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt) || Input.GetKey(KeyCode.AltGr))
+				{
+					SceneExporterGUI.Toggle();
+				} else {
+					if (Settings.IsEnabled)
+					{
+						activeDirectory = Path.Combine(Settings.ScenesDirectory, DateTime.Now.ToString("yyMMdd-HHmmss"));
+						//if (!Directory.Exists(activeDirectory)) Directory.CreateDirectory(activeDirectory);
+						if (Settings.IsExportingTextures)
+						{
+							texturesDirectory = Path.Combine(activeDirectory, "textures");
+							if (!Directory.Exists(texturesDirectory)) Directory.CreateDirectory(texturesDirectory);
+						}
+						DoExport();
+					}
+				}
+			}
+		}
+
+		void DoExport()
+		{
+
+			sb.Clear();
+			sbMaterials.Clear();
+			materialsCache.Clear();
+			vertexIndexOffset = gameObjectIndex = textureIndex = 0;
+
+			sb.AppendLine("mtllib scene.mtl");
+
+			foreach (GameObject go in FindObjectsOfType<GameObject>())
+			{
+				if (go.activeInHierarchy && go.scene.isLoaded && go.transform.position.magnitude < Settings.DistanceMax)
+				{
+					MeshFilter meshFilter = go.GetComponent<MeshFilter>();
+					MeshRenderer meshRenderer = go.GetComponent<MeshRenderer>();
+					if (meshFilter != null && meshRenderer != null)
+					{
+						DoExportMesh(go, meshFilter.sharedMesh, meshRenderer);
+						DoExportMaterials(go, meshRenderer);
+					}
+					SkinnedMeshRenderer[] skinnedMeshRenderers = go.GetComponents<SkinnedMeshRenderer>();
+					foreach (SkinnedMeshRenderer skinnedMeshRenderer in skinnedMeshRenderers)
+					{
+						Mesh meshBaked = new Mesh();
+						skinnedMeshRenderer.BakeMesh(meshBaked);
+						DoExportMesh(go, meshBaked, skinnedMeshRenderer);
+						DoExportMaterials(go, skinnedMeshRenderer);
+					}
+				}
+			}
+
+			//File.WriteAllText(Path.Combine(activeDirectory, "scene.obj"), sb.ToString());
+			//File.WriteAllText(Path.Combine(activeDirectory, "scene.mtl"), sbMaterials.ToString());
+			File.WriteAllText("scene.obj", sb.ToString());
+			File.WriteAllText("scene.mtl", sbMaterials.ToString());
+
+		}
+
+		void DoExportMesh(GameObject go, Mesh mesh, Renderer renderer)
+		{
+
+			sb.AppendLine(String.Format("g {0}_{1}", ReplaceSpaces(go.name), gameObjectIndex++));
+
+			foreach (Vector3 vertex in mesh.vertices)
+			{
+				//Vector3 v = go.transform.TransformPoint(vertex);
+				Vector3 v = go.transform.position + go.transform.rotation * Vector3.Scale(vertex, go.transform.lossyScale);
+				sb.AppendLine(String.Format("v {0} {1} {2}", -v.x, v.y, v.z));
+			}
+			foreach (Vector3 normal in mesh.normals)
+			{
+				Vector3 v = go.transform.TransformDirection(normal);
+				sb.AppendLine(String.Format("vn {0} {1} {2}", -v.x, v.y, v.z));
+			}
+			foreach (Vector2 v in mesh.uv)
+			{
+				sb.AppendLine(String.Format("vt {0} {1}", v.x, v.y));
+			}
+			for (int i = mesh.uv.Length; i < mesh.vertices.Length; i++)
+			{
+				sb.AppendLine("vt 0 0"); // for meshes without (?) uv map
+			}
+
+			for (int i = 0; i < mesh.subMeshCount; i++)
+			{
+				if (i < renderer.sharedMaterials.Length)
+				{
+					sb.AppendLine(String.Format("usemtl {0}", ReplaceSpaces(renderer.sharedMaterials[i].name)));
+					sb.AppendLine(String.Format("usemap {0}", ReplaceSpaces(renderer.sharedMaterials[i].name)));
+				} else {
+					sb.AppendLine(String.Format("usemtl {0}_{1}", ReplaceSpaces(go.name), i));
+					sb.AppendLine(String.Format("usemap {0}_{1}", ReplaceSpaces(go.name), i));
+				}
+				int[] triangles = mesh.GetTriangles(i);
+				for (int t = 0; t < triangles.Length; t += 3)
+				{
+					sb.AppendLine(String.Format("f {0}/{0}/{0} {1}/{1}/{1} {2}/{2}/{2}", triangles[t] + 1 + vertexIndexOffset, triangles[t+1] + 1 + vertexIndexOffset, triangles[t+2] + 1 + vertexIndexOffset));
+				}
+			}
+
+			vertexIndexOffset += mesh.vertices.Length;
+
+		}
+
+		void DoExportMaterials(GameObject go, Renderer renderer)
+		{
+
+			Material[] materials = renderer.sharedMaterials;
+			foreach (Material material in materials)
+			{
+
+				sbMaterials.AppendLine("newmtl " + ReplaceSpaces(material.name));
+				if (material.HasProperty("_Color"))
+				{
+					sbMaterials.AppendLine(String.Format("Kd {0} {1} {2}", material.color.r, material.color.g, material.color.b));
+					sbMaterials.AppendLine(String.Format("Tr {0}", 1f - material.color.a));
+				}
+				if (material.HasProperty("_SpecColor"))
+				{
+					Color specColor = material.GetColor("_SpecColor");
+					sbMaterials.AppendLine(String.Format("Ks {0} {1} {2}", specColor.r, specColor.g, specColor.b));
+				}
+
+				if (Settings.IsExportingTextures)
+				{
+					if (material.HasProperty("_MainTex"))
+					{
+						sbMaterials.AppendLine(String.Format("map_Kd {0}_MainTex.png", ReplaceSpaces(material.name)));
+					}
+					if (material.HasProperty("_SpecMap") || material.HasProperty("_Emissive"))
+					{
+						sbMaterials.AppendLine(String.Format("map_Ks {0}_SpecMap.png", ReplaceSpaces(material.name)));
+					}
+					if (material.HasProperty("_BumpMap"))
+					{
+						sbMaterials.AppendLine(String.Format("map_Bump {0}_BumpMap.png", ReplaceSpaces(material.name)));
+					}
+
+					if (!materialsCache.Contains(material.name))
+					{
+						materialsCache.Add(material.name);
+						string[] allTextureNames = material.GetTexturePropertyNames();
+						foreach (string textureName in allTextureNames)
+						{
+							Texture texture = material.GetTexture(textureName);
+							if (texture != null)
+							{
+								File.WriteAllBytes(Path.Combine(texturesDirectory, String.Format("{0}{1}_{2}.png", go.name, ReplaceSpaces(textureName), textureIndex++)), TextureToTexture2D(texture).EncodeToPNG());
+							}
+						}
+					}
+				}
+
+				sbMaterials.AppendLine("illum 2");
+
+			}
+
+		}
+
+		Texture2D TextureToTexture2D(Texture texture)
+		{
+			Texture2D texture2D = new Texture2D(texture.width, texture.height, TextureFormat.RGBA32, false);
+			RenderTexture renderTexture = new RenderTexture(texture.width, texture.height, 32);
+			Graphics.Blit(texture, renderTexture);
+			RenderTexture.active = renderTexture;
+			texture2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+			texture2D.Apply();
+			return texture2D;
+		}
+
+		string ReplaceSpaces(string str)
+		{
+			return str.Replace(" ", "_");
+		}
+
+		void Awake()
+		{
+			DontDestroyOnLoad(gameObject);
+			Settings.Load();
+		}
+
+	}
+
+}
